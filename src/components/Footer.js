391410d95e4967559e24de687a20ee435a4cef4e
Unknown <gameboy95thang@gmail.com>
2019-10-20 20:25:09 -0500

    file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ const Footer = (props) => (
               </a>
             </li>
             <li>
-              <a href="../Files/AnonResume.pdf" download className="icon fa-file"> 
+              <a href="../../static/AnonResume.pdf" download className="icon fa-file"> 
                 <span className="label">Resume</span>
               </a>
             </li>
