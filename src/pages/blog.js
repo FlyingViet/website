578b437856a80c6a1e029143d7e1f6ba13a3fc9d
Unknown <gameboy95thang@gmail.com>
2020-05-04 13:50:08 -0500

    Resume Update

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const blogPage = () => (
+  <Layout>
+    <h1>Experimental Page</h1>
+    <p>Wow hey didn't expect you here</p>
+  </Layout>
+)
+
+export default blogPage
